d2179bfe4880a730498df2b151456d97eaabacfb
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-08-09 19:16:32 +0600

    MUI structure created

---
+++ test-project/src/App.jsx
@@ -0,0 +1,13 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import {Button} from "@mui/material";
+
+const App = () => {
+    return (
+        <>
+            <CssBaseline />
+            <Button> </Button>
+        </>
+    );
+}
+
+export default App;
