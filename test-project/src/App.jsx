6bcab7cc9f1ffd14caf909091a8f59d3a67e2d32
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-08-10 19:43:49 +0600

    mui style created

--- test-project/src/App.jsx
+++ test-project/src/App.jsx
@@ -1,13 +1,21 @@
-import CssBaseline from '@mui/material/CssBaseline';
-import {Button} from "@mui/material";
+import {Checkbox, Container} from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+
+
 
 const App = () => {
-    return (
-        <>
-            <CssBaseline />
-            <Button> </Button>
-        </>
-    );
-}
+  return (
+    <>
+      <CssBaseline />
+      <Container maxWidth="md">
+          <Checkbox id="male" defaultChecked size="large"/> <label htmlFor="male">male</label>
+          <Checkbox id="female" size="small" />  <label htmlFor="female">female</label>
+          <Checkbox id="others"/> <label htmlFor="others">others</label>
+
+      </Container>
+
+    </>
+  );
+};
 
 export default App;
