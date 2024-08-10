52042bd6b214eb32c3fe798f6e069ed0a3a1ce5e
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-08-10 21:26:32 +0600

    components

--- test-project/src/App.jsx
+++ test-project/src/App.jsx
@@ -1,16 +1,21 @@
 import {Checkbox, Container} from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
-
-
+import {green, lightBlue, pink, red, yellow} from "@mui/material/colors";
+import {Favorite, FavoriteBorder} from "@mui/icons-material";
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 
 const App = () => {
   return (
     <>
       <CssBaseline />
       <Container maxWidth="md">
-          <Checkbox id="male" defaultChecked size="large"/> <label htmlFor="male">male</label>
-          <Checkbox id="female" size="small" />  <label htmlFor="female">female</label>
-          <Checkbox id="others"/> <label htmlFor="others">others</label>
+          <Checkbox id="male"  size="large" icon={<FavoriteBorder />}  sx={{ color: lightBlue[300], '& .MuiSvgIcon-root': { fontSize: 80 }, '&.Mui-checked': {
+                  color: red[600],
+              },}}/> <label htmlFor="male">male</label>
+          <Checkbox id="female" size="small" sx={{ '& .MuiSvgIcon-root': { fontSize: 50 }, color:yellow [600],'&.Mui-checked': {
+                  color: pink[600],
+              },}} checkedIcon={<Favorite />}/>  <label htmlFor="female">female</label>
+          <Checkbox id="others"  sx={{ color:green[800], '&.Mui-checked': {color: green[400]}}}   icon={<BookmarkBorderIcon />}/> <label htmlFor="others" >others</label>
 
       </Container>
 
