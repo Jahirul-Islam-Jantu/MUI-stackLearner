aa2e15ce1a3a405b386def7f591e02f1e9ad9f9a
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-09-03 14:07:01 +0600

    get playlist

--- test-project/src/api/index.js
+++ test-project/src/api/index.js
@@ -1,4 +1,9 @@
-const getPlayList = (playListId, pageToken, result=[]) => {
+import axios from "axios";
 
+const key = 'AIzaSyDPqV1EeRKTDuz1RTt_WSWD1NfPKyU52Dc'
+
+
+const getPlayList = async (playListId, pageToken, result=[]) => {
+    const URL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${key}&part=id,contentDetails,snippet,status&maxResults=50&playlistId=${playListId}&pageToken=${pageToken}`;
 }
 export default getPlayList
